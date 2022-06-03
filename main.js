ef18b92e5fc1f8caa4e8197a930d70f856864e3e
Jodie Solomons <jodies383@gmail.com>
2022-06-03 14:47:17 +0200

    quotes added

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import './style.css'
 import Alpine from 'alpinejs'
 import {LoveCounter} from './love-counter';
+import Quotes from './quotes';
+import './quotes.css'
 
 window.Alpine = Alpine
+
+Alpine.data('quoteApp', Quotes)
 Alpine.data('loveCounter', LoveCounter);
 Alpine.start()
 
