3ffd6a3b59ea3ff22cef92c7736f00376d933b8b
Jodie Solomons <jodies383@gmail.com>
2022-06-21 15:04:38 +0200

    init

--- server/index.js
+++ server/index.js
@@ -1,41 +1,3 @@
-// const pg = require("pg")
-// const PgPromise = require("pg-promise")
-// const express = require('express');
-// const fs = require('fs');
-// require('dotenv').config()
-// const cors = require('cors')
-// const { default: axios } = require('axios');
-// const app = express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// const {ConnectionString} = require('connection-string');
-// const { DATABASE_URL } = process.env;
-// const cs = new ConnectionString(DATABASE_URL);
-// const jwt = require('jsonwebtoken');
-// const { get } = require("http");
-// app.use(express.static('public'));
-// app.use(cors());
-// function get_PostgreSQL_connection() {
-//     return {
-//         host: cs.hostname,
-//         port: cs.port,
-//         database: cs.path?.[0],
-//         user: cs.user,
-//         password: cs.password,
-//         ssl: DATABASE_URL.includes('localhost') ? false : {rejectUnauthorized: false},
-//         application_name: cs.params?.application_name
-//     };
-// }
-
-// const pgp = PgPromise({});
-
-// const db = pgp(get_PostgreSQL_connection());
-
-// app.use(express.static('public'))
-
-
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -60,9 +22,6 @@ if (process.env.NODE_ENV == 'production') {
 
 const db = pgp(config);
 
-// const FruitEaterService = require('./fruit-eater-service');
-// const fruitEaterService = FruitEaterService(db);
-
 // enable the req.body object - to allow us to use HTML forms
 // and when using POST requests
 app.use(express.json());
@@ -71,10 +30,6 @@ app.use(cors());
 
 const API = require('./api');
 API(app, db);
-
-
-
-
 
 //configure the port number using and environment number
 var portNumber = process.env.PORT || 4000;
