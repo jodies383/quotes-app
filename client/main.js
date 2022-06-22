c4e4d3789590c29c79f1829084a1ed7444fb86a8
Jodie Solomons <jodies383@gmail.com>
2022-06-22 15:41:06 +0200

    fixed loading hearts issue

--- client/main.js
+++ client/main.js
@@ -3,14 +3,11 @@ import Alpine from 'alpinejs'
 import {LoveCounter} from './love-counter';
 import Quotes from './quotes';
 import './quotes.css'
-import {Authorization} from './auth'
-
 
 window.Alpine = Alpine
 
 Alpine.data('quoteApp', Quotes)
 Alpine.data('loveCounter', LoveCounter);
-Alpine.data('auth', Authorization);
 Alpine.start()
 
 document.querySelector('#app').innerHTML = `
